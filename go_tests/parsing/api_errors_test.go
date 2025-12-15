0224d735d9893e462032a35caaa8cd548b537e1e
Tyler Butler <tyler@tylerbutler.com>
2025-12-14 19:33:31 -0800

    chore: tests

--- go_tests/parsing/api_errors_test.go
+++ go_tests/parsing/api_errors_test.go
@@ -2,7 +2,7 @@ package parsing_test
 
 import (
 	"testing"
-	
+
 	"github.com/ccl-test-data/test-runner/internal/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -12,21 +12,16 @@ import (
 // Suite: Flat Format
 // Version: 1.0
 
-
-
 // just_key_error_parse - function:parse
 func TestJustKeyErrorParse(t *testing.T) {
-	
 
 	ccl := mock.New()
 	input := `key`
-	
+
 	// Declare variables for reuse across validations
-	
-	
-	
+
 	var err error
-	
+
 	// Parse validation
 	parseResult, err := ccl.Parse(input)
 	require.NoError(t, err)
@@ -34,21 +29,17 @@ func TestJustKeyErrorParse(t *testing.T) {
 	assert.Equal(t, expected, parseResult)
 
 }
-
 
 // whitespace_only_error_parse - function:parse feature:whitespace
 func TestWhitespaceOnlyErrorParse(t *testing.T) {
-	
 
 	ccl := mock.New()
 	input := `   `
-	
+
 	// Declare variables for reuse across validations
-	
-	
-	
+
 	var err error
-	
+
 	// Parse validation
 	parseResult, err := ccl.Parse(input)
 	require.NoError(t, err)
@@ -56,21 +47,17 @@ func TestWhitespaceOnlyErrorParse(t *testing.T) {
 	assert.Equal(t, expected, parseResult)
 
 }
-
 
 // whitespace_only_error_ocaml_reference_parse - function:parse feature:whitespace
 func TestWhitespaceOnlyErrorOcamlReferenceParse(t *testing.T) {
-	
 
 	ccl := mock.New()
 	input := `   `
-	
+
 	// Declare variables for reuse across validations
-	
-	
-	
+
 	var err error
-	
+
 	// Parse validation
 	parseResult, err := ccl.Parse(input)
 	require.NoError(t, err)
@@ -78,21 +65,17 @@ func TestWhitespaceOnlyErrorOcamlReferenceParse(t *testing.T) {
 	assert.Equal(t, expected, parseResult)
 
 }
-
 
 // just_string_error_parse - function:parse
 func TestJustStringErrorParse(t *testing.T) {
-	
 
 	ccl := mock.New()
 	input := `val`
-	
+
 	// Declare variables for reuse across validations
-	
-	
-	
+
 	var err error
-	
+
 	// Parse validation
 	parseResult, err := ccl.Parse(input)
 	require.NoError(t, err)
@@ -101,21 +84,17 @@ func TestJustStringErrorParse(t *testing.T) {
 
 }
 
-
 // multiline_plain_error_parse - function:parse feature:multiline
 func TestMultilinePlainErrorParse(t *testing.T) {
-	
 
 	ccl := mock.New()
 	input := `val
   next`
-	
+
 	// Declare variables for reuse across validations
-	
-	
-	
+
 	var err error
-	
+
 	// Parse validation
 	parseResult, err := ccl.Parse(input)
 	require.NoError(t, err)
@@ -124,22 +103,18 @@ func TestMultilinePlainErrorParse(t *testing.T) {
 
 }
 
-
 // multiline_plain_nested_error_parse - function:parse feature:multiline
 func TestMultilinePlainNestedErrorParse(t *testing.T) {
-	
 
 	ccl := mock.New()
 	input := `
 val
   next`
-	
+
 	// Declare variables for reuse across validations
-	
-	
-	
+
 	var err error
-	
+
 	// Parse validation
 	parseResult, err := ccl.Parse(input)
 	require.NoError(t, err)
@@ -147,5 +122,3 @@ val
 	assert.Equal(t, expected, parseResult)
 
 }
-
-
